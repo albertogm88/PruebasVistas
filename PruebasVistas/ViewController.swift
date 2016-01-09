b6894017419af2a32994cb38e2a6dc143f984b29
Alberto <albertogm88@gmail.com>
2016-01-09 18:55:12 +0100

    Initial Commit

---
+++ PruebasVistas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebasVistas
+//
+//  Created by Alberto Garcia Mendez on 9/1/16.
+//  Copyright © 2016 Alberto Garcia Mendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
